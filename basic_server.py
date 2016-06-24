8502df11b44237048990a14488c2b006beb5dc8d
Tom Wallis <github@tomwallis.net>
2016-06-25 00:20:00 +0100

    autogenerated archive page

--- basic_server.py
+++ basic_server.py
@@ -1,5 +1,6 @@
 from tornado import ioloop, web
 import CommonMark as cm
+from glob import glob
 
 class PageCreator(object):
     def __init__(self, mdfile):
@@ -15,8 +16,9 @@ class PageCreator(object):
 
         markdown = ""
         filepath = "posts/" + mdf
-        if mdf[-3:] != ".md": filepath += ".md"
-            
+        if mdf[-3:] != ".md":
+            filepath += ".md"
+
         with open(filepath) as open_md:
             markdown += open_md.read()
 
@@ -27,7 +29,7 @@ class MainHandler(web.RequestHandler):
         parser = PageCreator('index.md')
         with open('index.md') as open_md:
             self.write(parser.preamble + cm.commonmark(open_md.read()) + parser.closing)
-        
+
 class MarkdownRenderingHandler(web.RequestHandler):
     def get(self, postname):
         parser = PageCreator(postname)
@@ -40,17 +42,36 @@ class TufteDeliverer(web.RequestHandler):
 
 class FontDeliverer(web.RequestHandler):
     def get(self, path):
-        print("delivering fonts!")
         with open('et-book/'+path) as font_file:
             self.write(font_file.read())
+
+class ArchiveHandler(web.RequestHandler):
+    def get(self):
+        posts = glob("posts/*.md")
+        output = '<html><head><link rel="stylesheet" href="tufte.css"/></head><body>'
+
+        archive_content = "# Archive\n\nHere's an archive of all of my published posts:\n\n"
+        if len(posts) > 0:
+            posts = [post[6:-3] for post in posts] # Strip file extensions and path from titles of posts
+            for post in posts:
+                archive_content += "* [" + post + "](" + post + ")\n"
+
+        archive_content += "\n\n"
+
+        output += cm.commonmark(archive_content)
+
+        output += '</body></html>'
+        self.write(output)
 
 
 def make_app():
     return web.Application([
         (r'/', MainHandler),
         (r'/tufte.css', TufteDeliverer),
+        (r'/archive', ArchiveHandler),
         (r'/et-book/([a-zA-Z/.-]*)', FontDeliverer),
-        (r'/([a-z]+)', MarkdownRenderingHandler), 
+        (r'/([a-zA-Z\-\_]+\.md)', MarkdownRenderingHandler), 
+        (r'/([a-zA-Z\-\_]+)', MarkdownRenderingHandler), 
         ])
 
 if __name__ == "__main__":
